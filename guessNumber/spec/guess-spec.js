31ce6d6e27cf3b830c6854ef3807e32a283546de
lzhangwei <zhangwei_999@foxmail.com>
2014-08-22 11:43:29 +0800

    update guess test

--- guessNumber/spec/guess-spec.js
+++ guessNumber/spec/guess-spec.js
@@ -4,18 +4,36 @@
 describe('guessNumber', function () {
 
     var input;
+    var answerGenerate;
+    var compareNumber;
+
     beforeEach(function () {
-        input = '1234';
+        input = '7234';
+        answerGenerate = new AnswerGenerate();
+        compareNumber = new CompareNumber();
     });
 
-    it('it should return 4A0B',function() {
-        var answerGenerate = new AnswerGenerate();
+    it('it should return 3A0B',function() {
+
         spyOn(answerGenerate, 'generate').and.returnValue('1234');
-        var compareNumber = new CompareNumber();
+
+        spyOn(compareNumber, 'compare').and.returnValue('3A0B');
+
         var guess = new Guess(answerGenerate,compareNumber);
         var tips = guess.guess(input);
-        var truetips = compareNumber.compare(answerGenerate.generate(),input);
-        expect(tips).toBe(truetips);
+
+        expect(tips).toBe('3A0B');
+
+    });
+
+    it('it should called compare method',function() {
+        spyOn(answerGenerate, 'generate').and.returnValue('1234');
+        spyOn(compareNumber, 'compare');
+
+        var guess = new Guess(answerGenerate,compareNumber);
+        guess.guess(input);
+
+        expect(compareNumber.compare).toHaveBeenCalledWith('1234',input);
     });
 
 });
